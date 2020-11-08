e64990591a71eab211aec2d1b2b8122aac587eb9
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-11-07 19:08:35 -0800

    deploying to heroku

--- server.js
+++ server.js
@@ -16,12 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
-});
+  }
+);
 
 // defining routes
 app.use(require("./routes/api.js"));
