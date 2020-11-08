f257689f165cbe13d48cfdbb7f1c89a9933e8883
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-11-07 17:00:44 -0800

    created service worker file.  Not fully functional

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/db.js",
+  "/index.html",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
