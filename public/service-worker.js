84761083a12566d5f4244ce2ec122c2feb22262b
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-11-07 17:23:49 -0800

    updated service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// files to cache
 const FILES_TO_CACHE = [
   "/",
   "/db.js",
@@ -9,3 +10,34 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
+
+// cacheing files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("cache successful");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+});
+
+// fetching cached files
+self.addEventListener("fetch", (event) => {
+  console.log(caches);
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+        })
+    );
+    return;
+  };
+});
